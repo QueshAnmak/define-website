115060bfa866e117476935f19d53c0993e51baa6
Samriddh Jain <samriddhjn@gmail.com>
2021-12-14 00:05:36 +0530

    modified:   src/components/EventCard.tsx 	modified:   src/index.css 	modified:   src/styles/event-card.scss 	modified:   src/styles/event-grid.scss

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,9 +1,14 @@
 import '../styles/event-card.scss'
 
-
 export default function EventCard() {
     return (
         <div className="event-card">
-</div>
+            <img className='event-card-image'
+                src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/snh_online_6072x9000_posed_01.jpg"
+                alt="event_cover_image" />
+            <div className="event-card-info">
+                
+            </div>
+        </div>
     );
 }
