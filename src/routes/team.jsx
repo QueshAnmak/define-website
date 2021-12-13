17f4368ebe4529f1e291d7788ea3c583529562ec
Samriddh Jain <samriddhjn@gmail.com>
2021-12-13 18:01:47 +0530

    added dhairya

--- src/routes/team.jsx
+++ src/routes/team.jsx
@@ -1,12 +1,17 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
 
+import '../styles/team-page.scss'
+
 export default function Team() {
 	return (
 		<div id="app">
 			<Header />
-			<section id="main"></section>
+			<section id="main">
+				
+				<h1 id='team-heading' className='one'>The Team</h1>
 
+			</section>
 			<Footer />
 		</div>
 	);
