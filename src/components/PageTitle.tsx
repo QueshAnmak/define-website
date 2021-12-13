38a7320b3dbfe4854028a02b02d73b78109c66c3
dhairyabahl <dhairyabahl5@gmail.com>
2021-12-13 15:47:50 +0000

    Added Files

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,8 +1,8 @@
 import "../styles/page-title.scss"
 
-export default function PageTitle( props ) {
+export default function PageTitle( props:any ) {
 
     return (
-        <h1 id='page-title' className='one'>{props.title}</h1>
+        <div id='page-title' className='one'>{props.title}</div>
     );
 }
